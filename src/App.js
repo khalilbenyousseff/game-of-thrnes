623031b3e529c7d097a008eef283e0d40a71757c
khalilbenyousseff <122831628+khalilbenyousseff@users.noreply.github.com>
2023-02-07 16:57:06 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import getItems from './pages/names';
 export default function App() {
+  // const apiBase = 'https://www.anapioficeandfire.com/api';
+  for (let i = 1; i <= 20; i++) {
+    getItems(i);
+  }
+  const [utilisateurs, setUtilisateurs] = useState([]);
+  const api = 'https://www.anapioficeandfire.com/api/characters';
+  useEffect(() => {
+    fetch(`${api}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setUtilisateurs(data));
+  }, [api]);
   const [books, setBooks] = useState(null);
-
   const fetchData = async () => {
-    const response = await axios.get(
-      'https://anapioficeandfire.com/api/books'
-    );
+    const response = await axios.get('https://anapioficeandfire.com/api/books');
 
     setBooks(response.data);
   };
 
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
       <h1>Game of Thrones</h1>
       <h2>Fetch the books</h2>
 
-     
       <div>
-        <button onClick={fetchData}>
-          Fetch Data
-        </button>
+        <button onClick={fetchData}>Fetch Data</button>
         <br />
       </div>
 
-      
       <div>
         {books &&
           books.map((book, index) => {
@@ -38,6 +44,12 @@ export default function App() {
 
                 <div>
                   <p>{authors}</p>
+                  <p>
+                    {utilisateurs.map((utilisateur, index) => {
+                      return <p key={index + 1}>{utilisateur.name}</p>;
+                    })}
+                  </p>
+                  <p></p>
                   <p>{book.characters}</p>
                   <p>{book.numberOfPages} pages</p>
                   <p>{book.country}</p>
