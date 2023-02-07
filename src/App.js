a12a2a172100ba243bc83f0b67b32d69416aa729
khalilbenyousseff <122831628+khalilbenyousseff@users.noreply.github.com>
2023-02-07 10:12:24 +0100

    Add files via upload
    code source App.js

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function App() {
+  const [books, setBooks] = useState(null);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      'https://www.anapioficeandfire.com/api/books'
+    );
+
+    setBooks(response.data);
+  };
+
+  return (
+    <div className="App">
+      <h1>Game of Thrones</h1>
+      <h2>Fetch the books</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button" onClick={fetchData}>
+          Fetch Data
+        </button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+      <div className="books">
+        {books &&
+          books.map((book, index) => {
+            const cleanedDate = new Date(book.released).toDateString();
+            const authors = book.authors.join(', ');
+
+            return (
+              <div className="book" key={index}>
+                <h3>Book {index + 1}</h3>
+                <h2>{book.name}</h2>
+
+                <div className="details">
+                  <p>{authors}</p>
+                  <p>{book.characters}</p>
+                  <p>{book.numberOfPages} pages</p>
+                  <p>{book.country}</p>
+                  <p>{cleanedDate}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
