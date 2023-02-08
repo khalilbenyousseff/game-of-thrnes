6adac0f065ce3c2cf9b21e670a70ee96f0f28bed
khalilbenyousseff <122831628+khalilbenyousseff@users.noreply.github.com>
2023-02-08 11:22:16 +0100

    Add files via upload
    j'ai pas voulu faire deux components j'ai travaillé dans une seul fichier app.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,104 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import getItems from './pages/names';
-export default function App() {
-  // const apiBase = 'https://www.anapioficeandfire.com/api';
-  for (let i = 1; i <= 20; i++) {
-    getItems(i);
-  }
-  const [utilisateurs, setUtilisateurs] = useState([]);
-  const api = 'https://www.anapioficeandfire.com/api/characters';
-  useEffect(() => {
-    fetch(`${api}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => setUtilisateurs(data));
-  }, [api]);
-  const [books, setBooks] = useState(null);
-  const fetchData = async () => {
-    const response = await axios.get('https://anapioficeandfire.com/api/books');
+import React, { useState, useEffect } from 'react';
 
-    setBooks(response.data);
-  };
+/*----------  BOOK --------------*/
+
+const BookList = ({ onBookSelect }) => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch('https://anapioficeandfire.com/api/books');
+        const data = await response.json();
+        setBooks(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Game of Thrones</h1>
-      <h2>Fetch the books</h2>
+    <ul>
+      {books.map((book) => (
+        <li key={book.url} onClick={() => onBookSelect(book)}>
+          {book.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-      <div>
-        <button onClick={fetchData}>Fetch Data</button>
-        <br />
-      </div>
+/*--------------- CHARACTER ----------*/
 
-      <div>
-        {books &&
-          books.map((book, index) => {
-            const authors = book.authors.join(', ');
+const CharacterList = ({ bookId }) => {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-            return (
-              <div key={index}>
-                <h3>Book {index + 1}</h3>
-                <h2>{book.name}</h2>
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await fetch(
+          `https://anapioficeandfire.com/api/books/${bookId}/characters`
+        );
+        const data = await response.json();
+        setCharacters(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchCharacters();
+  }, [bookId]);
 
-                <div>
-                  <p>{authors}</p>
-                  <p>
-                    {utilisateurs.map((utilisateur, index) => {
-                      return <p key={index + 1}>{utilisateur.name}</p>;
-                    })}
-                  </p>
-                  <p></p>
-                  <p>{book.characters}</p>
-                  <p>{book.numberOfPages} pages</p>
-                  <p>{book.country}</p>
-                </div>
-              </div>
-            );
-          })}
-      </div>
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <ul>
+      {characters.map((character) => (
+        <li key={character.url}>{character.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+/*------------APP------------*/
+
+const App = () => {
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  return (
+    <div>
+      <h1>Books</h1>
+      <BookList onBookSelect={setSelectedBook} />
+      {selectedBook && (
+        <>
+          <h1>Characters</h1>
+          <CharacterList bookId={selectedBook.url} />
+        </>
+      )}
     </div>
   );
-}
+};
+
+export default App;
